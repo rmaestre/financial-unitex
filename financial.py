ab3a411854a8f68119926a693a96cb74b90c43a5
root <root@web40.(none)>
2012-06-25 10:23:36 +0200

    Manage of extracted data (top-5) and tweeting

--- financial.py
+++ financial.py
@@ -2,7 +2,11 @@
 # coding: utf-8
 
 from UnitexManager import UnitexManager
+
 import sys
+import twitter
+from operator import itemgetter
+
 
 try:
     # version 2.6+
@@ -11,21 +15,30 @@ except:
     # version 2.5
     import simplejson as json
     
-DEFAULT_FILENAME = "testData.json"
+DEFAULT_FILENAME = "../data/NASDAQ_TOP10.json" 
+
 
 def main(argv):
-    print "Starting process..."
+    print "Starting process..." 
 
-    companyRawData = parseFile(DEFAULT_FILENAME)
-    #print "Companies Are %s" %companyRawData
-    companyData = json.loads(companyRawData.replace('=>', ':'))
+    api = twitter.Api(consumer_key = 'THE KEY' , consumer_secret='THE GOOD',
+		 access_token_key='THE BAD', access_token_secret='THE UGLY')
     
+    print api.VerifyCredentials()
+	
+    companyRawData = parseFile(DEFAULT_FILENAME)
+    print "Companies Are %s" %companyRawData
+    companyData = json.loads(companyRawData.replace('=>', ':'))
+
+
     companyText = ""
     companyElements = []
     for element in companyData:
         test = '%s %s %s' %(element["arrow"], element["change"], element["code"])
         companyElements.append(test)
-        
+   
+    extractedData = []        
+
     for companyLine in companyElements:
         print " -> Processing: %s " % companyLine
         unitexManager = UnitexManager()
@@ -36,6 +49,26 @@ def main(argv):
         # Apply Grammar
         grammar = unitexManager.grammar(tokens_result, pos_tagging, "en")
         print grammar
+	extractedData.append(json.loads(grammar))
+
+    print "Extracted data is:\n%s" %extractedData
+
+    stocks = sorted(extractedData, key=itemgetter('number'), reverse=True)   
+    print "Ordered Stocks are:\n%s" %stocks
+
+    mostActive = stocks[0:4] 
+    print "Most active are:\n%s" %mostActive
+
+    msg = "The most active values in NASDAQ are "
+    for stock in mostActive:
+	sign = u"↑" if  stock['operator'] == "up" else u"↓";
+	msg = "%s %s: %s %s;" %(msg, stock['symbol'], stock['number'], sign)
+
+    print "Msg is [%s]" %msg 	   	
+    status = api.PostUpdate(msg)
+    print status
+	
+	
 def parseFile(filename):
     file = open(filename, 'r')
     return file.read()
